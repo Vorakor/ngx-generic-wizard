1ee47184d170f34c57ea439d53673170f3fc2a8b
Shaine Berube <ShaineB@natr-temps.com>
2019-12-02 15:33:47 -0700

    Adding comment

--- projects/npx-generic-wizard/src/lib/npx-generic-wizard.module.ts
+++ projects/npx-generic-wizard/src/lib/npx-generic-wizard.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
-import { NpxGenericWizardComponent } from './npx-generic-wizard.component';
+import { NgModule } from "@angular/core";
+import { NpxGenericWizardComponent } from "./npx-generic-wizard.component";
 
-
+// Build in such a way that it doesn't matter if it's Material or Bootstrap that someone chooses...
 
 @NgModule({
   declarations: [NpxGenericWizardComponent],
-  imports: [
-  ],
+  imports: [],
   exports: [NpxGenericWizardComponent]
 })
-export class NpxGenericWizardModule { }
+export class NpxGenericWizardModule {}
