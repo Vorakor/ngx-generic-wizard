96515e8590e4499f1b3522c30e8893b316dc8c24
Shaine Berube <ShaineB@natr-temps.com>
2019-12-05 14:27:48 -0700

    Hopefully finished off the code, except for color schemes.

--- projects/ngx-generic-wizard/src/lib/ngx-generic-wizard-button-container/ngx-generic-wizard-button-container.component.ts
+++ projects/ngx-generic-wizard/src/lib/ngx-generic-wizard-button-container/ngx-generic-wizard-button-container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { INgxGwConfig, NgxGenericWizardService } from '../../public-api';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { map, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-gw-button-container',
@@ -12,11 +13,50 @@ export class NgxGenericWizardButtonContainerComponent implements OnInit {
   finalize$: Observable<boolean> = this.ngxGwService.finalized$;
   nextBtnText: string;
   prevBtnText: string;
+  prevBtnShow: boolean;
   reenterBtnText: string;
   constructor(private ngxGwService: NgxGenericWizardService) {}
 
   ngOnInit() {
-    //
+    if (!this.config) {
+      throw new Error('Button container needs an assigned configuration');
+    }
+    this.prevBtnText = 'Previous';
+    this.reenterBtnText = 'Re-enter Wizard';
+    const wzBtnSub = combineLatest(
+      this.ngxGwService.ngxGwSteps$.pipe(
+        map(steps =>
+          steps.filter(step => step.configId === this.config.configId)
+        ),
+        distinctUntilChanged()
+      ),
+      this.ngxGwService.wizardStepStatusMap$.pipe(distinctUntilChanged())
+    )
+      .pipe(map(([st, statMap]) => ({ Steps: st, StatusMap: statMap })))
+      .subscribe(results => {
+        const currentStep = results.Steps.filter(
+          step => step.status.code === results.StatusMap.current.code
+        )[0];
+        const maxOrder: number = Math.max.apply(
+          Math,
+          results.Steps.map(step => step.stepOrder)
+        );
+        const minOrder: number = Math.min.apply(
+          Math,
+          results.Steps.map(step => step.stepOrder)
+        );
+        if (currentStep && currentStep.stepOrder === maxOrder) {
+          this.nextBtnText = 'Finish';
+        } else {
+          this.nextBtnText = 'Next';
+        }
+        if (currentStep && currentStep.stepOrder === minOrder) {
+          this.prevBtnShow = false;
+        } else {
+          this.prevBtnShow = true;
+        }
+      });
+    this.ngxGwService.addSubscription(wzBtnSub);
   }
 
   action(event) {
--- projects/ngx-generic-wizard/src/lib/ngx-generic-wizard-step-container/ngx-generic-wizard-step-container.component.ts
+++ projects/ngx-generic-wizard/src/lib/ngx-generic-wizard-step-container/ngx-generic-wizard-step-container.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgxGenericWizardService } from '../../public-api';
+import { Observable } from 'rxjs';
+import { INgxGwStep } from '../interfaces';
 
 @Component({
   selector: 'ngx-gw-step-container',
   templateUrl: './ngx-generic-wizard-step-container.component.html',
   styleUrls: ['./ngx-generic-wizard-step-container.component.scss']
 })
-export class NgxGenericWizardStepContainerComponent {
+export class NgxGenericWizardStepContainerComponent implements OnInit {
+  steps$: Observable<INgxGwStep[]> = this.ngxGwService.ngxGwSteps$;
   constructor(private ngxGwService: NgxGenericWizardService) {}
+
+  ngOnInit() {
+    const wzStepSub = this.ngxGwService.initialized$.subscribe(init => {
+      if (!init) {
+        throw new Error('Need to initialize the wizard generator!');
+      }
+    });
+    this.ngxGwService.addSubscription(wzStepSub);
+  }
 
   navigate(event) {
     this.ngxGwService.navigateToStep(event);
